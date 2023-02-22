3f0c1b6b6b051e34ff0a5997b7d023cff131e8d0
Kushagra Juneja <121512664+ku28@users.noreply.github.com>
2023-02-22 23:04:14 +0530

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -1,25 +1,5 @@
 #include<stdio.h>
 int main() {
-    char ch;
-    printf("enter ch: ");
-    scanf("%c",&ch);
-    switch(ch){
-        case'a':
-        printf("vowel");
-        break;
-        case'e':
-        printf("vowel");
-        break;
-        case'i':
-        printf("vowel");
-        break;
-        case'o':
-        printf("vowel");
-        break;
-        case'u':
-        printf("vowel");
-        break;
-        default:
-        printf("consonant");
-    }
+    printf("Hello World");
+    return 0;
 }
